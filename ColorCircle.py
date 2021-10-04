d906bcb9435bed7633a7b685e1409b4f5af3b06b
v1233c <91565733+v1233c@users.noreply.github.com>
2021-10-04 15:03:20 -0500

    random color circle

---
+++ ColorCircle.py
@@ -0,0 +1,15 @@
+#Initialization
+import turtle
+vicky = turtle.Turtle()
+import random
+
+#Functions
+
+def randomDot():
+    vicky.color("green")
+    vicky.begin_fill()
+    vicky.circle(100)
+    vicky.end_fill()
+    
+#Main
+randomDot()
