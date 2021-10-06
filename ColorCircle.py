ccc2c4af5ee79982d281f2a59a66cb5c0d3f4722
v1233c <91565733+v1233c@users.noreply.github.com>
2021-10-06 14:43:02 -0500

    Update ColorCircle.py

--- ColorCircle.py
+++ ColorCircle.py
@@ -2,14 +2,27 @@
 import turtle
 vicky = turtle.Turtle()
 import random
+turtle.colormode(255)
+vicky.speed(100)
 
 #Functions
 
 def randomDot():
-    vicky.color("green")
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    c = random.randint(0,100)
+    vicky.color(r,g,b)
     vicky.begin_fill()
-    vicky.circle(100)
+    vicky.circle(c)
     vicky.end_fill()
+  
     
 #Main
-randomDot()
+for i in range(100):
+    x = random.randint(-300,300)
+    y = random.randint(-300,300)
+    vicky.penup()
+    vicky.goto(x,y)
+    vicky.pendown()
+    randomDot()
